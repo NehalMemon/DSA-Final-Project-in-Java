ebbcc29ec661d3b7504b71df7728edaf730e4ce2
Nehal <nehalmemon73@gmail.com>
2025-11-18 19:51:56 +0500

    improve folder structure

---
+++ Main.java
@@ -0,0 +1,14 @@
+import ui.MainWindow;
+
+import javax.swing.SwingUtilities;
+
+public class Main {
+
+    public static void main(String[] args) {
+     
+        SwingUtilities.invokeLater(() -> {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.setVisible(true);
+        });
+    }
+}
--- src/Main.java
+++
@@ -1,8 +0,0 @@
-import ui.AddTaskForm;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("System Initiated");
-        new AddTaskForm();
-    }
-}
